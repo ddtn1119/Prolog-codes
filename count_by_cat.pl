49e9de2b45d97dfe0320e89bb13031647ce659e9
Andy Nguyen <106499410+ddtn1119@users.noreply.github.com>
2024-11-25 12:28:09 -0600

    Update count_by_cat.pl

--- count_by_cat.pl
+++ count_by_cat.pl
@@ -1,15 +1,11 @@
-% Andy Nguyen
-% CS 4337.502
-% Homework 5 Problem 10
-
 % helper predicate with accumulators for sumkey0 and sumkey1
-% unify results at end of the list
+% unify results at the end of the list
 count_by_cat_helper([], Sum0, Sum1, Sum0, Sum1).
-% add value to accumulator for key 0
+% add value to the accumulator for key 0
 count_by_cat_helper([pair(0, Value) | Tail], Acc0, Acc1, Sum0, Sum1) :-
     NewAcc0 is Acc0 + Value,
     count_by_cat_helper(Tail, NewAcc0, Acc1, Sum0, Sum1).
-% add value to accumulator for key 1
+% add value to the accumulator for key 1
 count_by_cat_helper([pair(1, Value) | Tail], Acc0, Acc1, Sum0, Sum1) :-
     NewAcc1 is Acc1 + Value,
     count_by_cat_helper(Tail, Acc0, NewAcc1, Sum0, Sum1).
